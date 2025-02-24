cbe71bff76e53342fd6e68d4fa287b7ac1d73822
rony <siamore9724@gmail.com>
2025-02-24 18:50:45 +0900

    조커 도둑의 반지 추가

---
+++ Assets/Scripts/InGame/CardSystem/CE/CE_GrantGold.cs
@@ -0,0 +1,35 @@
+using Cysharp.Threading.Tasks;
+using TSoft.InGame.GamePlaySystem;
+
+namespace TSoft.InGame.CardSystem.CE
+{
+    public class CE_GrantGold : CustomEffect
+    {
+        public override async UniTask ApplyEffect(InGameDirector director, Gameplay.AppliedGameplayEffect sourceEffect)
+        {
+            var currentGold = GameSave.Instance.Gold;
+        
+            foreach (var modifier in sourceEffect.sourceEffect.gameplayEffect.modifiers)
+            {
+                var magnitude = (int)modifier.gameplayMagnitude.magnitude;
+            
+                switch (modifier.modifierOp)
+                {
+                    case ModifierOpType.Add:
+                        currentGold += magnitude;
+                        break;
+                    case ModifierOpType.Multiply:
+                        currentGold *= magnitude;
+                        break;
+                    case ModifierOpType.Divide:
+                        currentGold /= magnitude;
+                        break;
+                }
+            }
+        
+            GameSave.Instance.SetGold(currentGold);
+        
+            await UniTask.CompletedTask;
+        }
+    }
+}
