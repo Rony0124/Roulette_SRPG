c482ed1b74f8f483d68a4a0d761c74bbaf5bc8aa
rony <siamore9724@gmail.com>
2025-03-13 18:04:27 +0900

    text 움직임 개선

--- Assets/Scripts/UI/Views/InGame/FloatingUIText.cs
+++ Assets/Scripts/UI/Views/InGame/FloatingUIText.cs
@@ -1,4 +1,5 @@
 using System;
+using MoreMountains.Tools;
 using TMPro;
 using UnityEngine;
 
@@ -7,10 +8,47 @@ namespace TSoft.UI.Views.InGame
     public class FloatingUIText : MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI tmp;
+        [SerializeField] private AnimationCurve animateYCurve;
+        [SerializeField] private float lifeTime;
+        [SerializeField] private float remapYZero;
+        [SerializeField] private float remapYOne;
 
         public TextObjectPool pool { get; set; }
 
+        private float startedAt;
+        private float elapsedTime;
+        private float remappedTime;
+        private Vector3 newPosition;
         private bool isPooled;
+
+        public virtual float GetTime() => Time.unscaledTime;
+
+        private void Update()
+        {
+            if (!isActiveAndEnabled)
+                return;
+            
+            elapsedTime = GetTime() - startedAt;
+            remappedTime = MMMaths.Remap(elapsedTime, 0f, lifeTime, 0f, 1f);
+
+            HandleMovement();
+        }
+        
+        protected virtual void HandleMovement()
+        {
+            transform.up = Vector3.up;
+
+            var newPosition = transform.localPosition;
+            newPosition.y += MMMaths.Remap(animateYCurve.Evaluate(remappedTime), 0f, 1, remapYZero, remapYOne);
+            
+            transform.localPosition = newPosition;
+        }
+
+
+        private void OnEnable()
+        {
+            startedAt = GetTime();
+        }
 
         public void SetText(string content, Color color)
         {
