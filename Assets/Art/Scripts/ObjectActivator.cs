75d7990ba87f027ca5d929f0a5e22acd42adb671
WooJinJeon <s.bellcranell@gmail.com>
2025-03-28 15:10:05 +0900

    lobby 작업

--- Assets/Art/Scripts/ObjectActivator.cs
+++ Assets/Art/Scripts/ObjectActivator.cs
@@ -6,33 +6,35 @@ using UnityEngine.UI;
 
 public class ObjectActivator : MonoBehaviour
 {
-    // ��ư�� ������ ����
+    // 버튼을 연결할 변수
     public Button yourButton;
-    // Ȱ��ȭ�� ������Ʈ���� �迭�� ����
+    // 활성화할 오브젝트들을 배열로 설정
     public GameObject[] objectsToActivate;
-    // ���� Ȱ��ȭ�� ������Ʈ�� �ε����� �����ϴ� ����
+    // 현재 활성화할 오브젝트의 인덱스를 추적하는 변수
     private int currentIndex = 0;
 
     void Start()
     {
-        // ��ư Ŭ�� �� ActivateNextObject �Լ� ȣ��
+        // 버튼 클릭 시 ActivateNextObject 함수 호출
         yourButton.onClick.AddListener(() => StartCoroutine(ActivateNextObject()));
     }
 
-    // ��ư Ŭ�� �� �ϳ��� ������Ʈ�� Ȱ��ȭ�ϴ� �ڷ�ƾ
+    // 버튼 클릭 시 하나씩 오브젝트를 활성화하는 코루틴
     IEnumerator ActivateNextObject()
     {
-        // ���� Ȱ��ȭ�� ������Ʈ�� �ִٸ�
+        // 아직 활성화할 오브젝트가 있다면
         if (currentIndex < objectsToActivate.Length)
         {
-            objectsToActivate[currentIndex].SetActive(true);  // ���� �ε��� ������Ʈ Ȱ��ȭ
-            currentIndex++;  // ���� ������Ʈ�� �̵�
-            yield return new WaitForSeconds(0.5f);  // 0.5�� ���
+            objectsToActivate[currentIndex].SetActive(true);  // 현재 인덱스 오브젝트 활성화
+            currentIndex++;  // 다음 오브젝트로 이동
+            yield return new WaitForSeconds(0.5f);  //  0.5초 대기
         }
     }
 
     public async UniTask ReturnActivatedCards()
     {
+        currentIndex = 0;
+
         float duration = objectsToActivate[0].GetComponent<ActivatedCard>().FeelDuration;
         bool shouldWait = false;
         foreach (var obj in objectsToActivate)
